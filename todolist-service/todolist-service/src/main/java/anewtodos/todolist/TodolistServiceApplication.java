b37706669716cfe01d2433eb09d22636e5708db3
MarcoFierimonte <marco.fierimonte@gmail.com>
2019-09-10 23:33:52 +0200

    added spring boot application todolist-service

---
+++ todolist-service/todolist-service/src/main/java/anewtodos/todolist/TodolistServiceApplication.java
@@ -0,0 +1,13 @@
+package anewtodos.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodolistServiceApplication.class, args);
+    }
+
+}
